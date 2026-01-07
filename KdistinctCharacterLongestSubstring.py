f18ff1a1f02499c4484685b37aba66ed78f592f2
Sandip Nandi <59725621+SandipNandy@users.noreply.github.com>
2026-01-07 12:40:33 +0530

    Update KdistinctCharacterLongestSubstring.py

--- KdistinctCharacterLongestSubstring.py
+++ KdistinctCharacterLongestSubstring.py
@@ -2,6 +2,7 @@
 """
  Problem: Longest Substring with At Most K Distinct Characters
 Question:
+
 Given a string s and an integer K, find the length of the longest substring T that contains at most K distinct characters.
 Constraints:
 1≤length of s≤5×10 
@@ -94,6 +95,7 @@ Example 7 (Edge Case)
 
 The longest substring is any single character (e.g., "a" or "b").
 """
+
 
 
 
